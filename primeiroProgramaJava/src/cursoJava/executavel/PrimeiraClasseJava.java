b2b81d30f136284de1cdf83c9db9a382308b52e4
andre <andre@Andr>
2022-10-22 18:26:39 -0300

    O tipo CHAR de caracteres

--- primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
+++ primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -4,18 +4,16 @@ public class PrimeiraClasseJava {
 	
 	public static void main(String[] args) {
 		
-		double nota1 = 90;
-		double nota2 = 70;
-		double nota3 = 50;
-		double nota4 = 88;
+		/* CHAR representa uma letra*/
+		char pessoaFeminino = 'F';
 		
-		double mediaFinal =  (nota1 + nota2 + nota3 + nota4)/4;
-		
-		System.out.println("Média das notas: " + mediaFinal);
+		if(pessoaFeminino == 'F') {
+			System.out.println("Feminino");
+		}else {
+			System.out.println("masculino");
+		}
 	}
 	
-	
-
 	
 }
 	
