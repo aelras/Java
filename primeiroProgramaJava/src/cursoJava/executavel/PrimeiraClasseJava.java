0f5fc3f0249127f66fb3d86e3ae606eeef83f70b
andre <andre@Andr>
2022-10-24 19:58:13 -0300

    SWITCH CASE: OPERAÇÕES EXATAS

--- primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
+++ primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -4,27 +4,20 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		int nota1 = 70;
-		int nota2 = 80;
-		int nota3 = 70;
-		int nota4 = 60;
-		int media = 0;
-
-		media = (nota1 + nota2 + nota3 + nota4) / 4;
-		
-		System.out.println("A média é: " + media);
-		/* Operações lógicas aninhadas: São operações dentro de operações*/
-		
-		if (media >= 50) {
-			if (media >= 70) {
-				if (media > 90) {
-					System.out.println("Aluno está aprovado! Parabéns");
-				} else {
-					System.out.println("Aluno está aprovado!");
-				}
-			} else {
-				System.out.println("Aluno está em recuperação!");
-			}
+		/* SWITCH CASE: OPERAÇÕES EXATAS*/
+		int dia = 4;
+		switch (dia) {
+		case 1: 
+			System.out.println("domingo");
+			break;
+		case 2: 
+			System.out.println("segunda-feira");
+			break;
+		case 3: 
+			System.out.println("terça-feira");
+			break;
+		default: System.out.println("Outro dia quaquer" );
+			break;
 		}
 	}
 }
