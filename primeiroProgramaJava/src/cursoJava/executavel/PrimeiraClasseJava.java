6b536c085d54467f7cda90b38b8eeae30673b30b
andre <andre@Andr>
2022-10-24 21:07:07 -0300

    Operadores relacionais

--- primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
+++ primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -4,20 +4,16 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		/* SWITCH CASE: OPERAÇÕES EXATAS*/
-		int dia = 2;
-		switch (dia) {
-		case 1: 
-			System.out.println("domingo");
-			break;
-		case 2: 
-			System.out.println("segunda-feira");
-			break;
-		case 3: 
-			System.out.println("terça-feira");
-			break;
-		default: System.out.println("Outro dia quaquer" );
-			break;
+		/* =, ==, !, <, >, <=, >=*/
+		int nota1 = 91;
+		int nota2 = 91;
+		
+		if (nota1 != nota2) {
+			System.out.println("As notas são diferentes");
+		}else {
+			System.out.println("As notas são iguais");
 		}
-	}
+		
+		}
+
 }
