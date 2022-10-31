e59df5702776784dccc58a9ac7abe82a335ba5ec
andre <andre@Andre>
2022-10-31 18:21:06 -0300

    while verifica e depois executa o código Do while primeiro executa e depois verifica
    Ambos só terminam quando o resultado der False

--- primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
+++ primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -4,16 +4,23 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		/* =, ==, !, <, >, <=, >=*/
-		int nota1 = 91;
-		int nota2 = 91;
+		/*Estrutura de repetição WHILE*/
 		
-		if (nota1 != nota2) {
-			System.out.println("As notas são diferentes");
-		}else {
-			System.out.println("As notas são iguais");
-		}
+		int numero = 0;
 		
+		while(numero <= 60) { /*Verifica e depois executa o código*/
+			
+			System.out.println("O número atual é : " + numero);
+			numero ++; // acrecenta um número a cada iteração
 		}
+		/*--------------------*/
+		
+		int numero2 = 0;
+		do { /*Primeiro executa e depois verifica*/
+			
+			System.out.println("O número atual é : " + numero2);
+			numero2 ++;
+		}while(numero2 <= 60);
+	}
 
 }
