2c2daba5c23b766772e0119b984f5e347d86e68d
andre <andre@Andre>
2022-11-01 16:44:16 -0300

    Divisões e resto com MOD do Java

--- primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
+++ primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -4,18 +4,14 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		/*Estrutura de repetição FOR e CONTINUE*/
+		/*Módulo: Resto da divisão*/
 		
-		for (int numero = 0; numero <= 10; numero ++) {
-			if (numero == 3 || numero == 6|| numero == 9  ) {
-				System.out.println("Obaaa, encontrei o número " + numero);
-				continue;
-			}
-			
-			System.out.println("Processando laço de repetição ");
-		}
+		double carro = 9;
+		double pessoa = 2;
 		
+		double resto = carro % pessoa;
 		
+		System.out.println("Sobraram exatamente: " + resto + " carros");
 	}
 
 }
