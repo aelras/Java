68113775f63478534ef2fa43bfacc43b3e1a89fe
andre <andre@Andre>
2022-11-01 17:00:56 -0300

    Criando a entrada e a saída de dados

--- primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
+++ primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -1,17 +1,24 @@
 package cursoJava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
 
+	
 	public static void main(String[] args) {
 
-		/*Módulo: Resto da divisão*/
 		
-		double carro = 9;
-		double pessoa = 2;
+		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros: ");
+		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas: ");
 		
-		double resto = carro % pessoa;
+		double carroNumero = Double.parseDouble(carros);
+		double pessoaNumero = Double.parseDouble(pessoas);
 		
-		System.out.println("Sobraram exatamente: " + resto + " carros");
+		int divisao = (int)(carroNumero / pessoaNumero);
+		
+		double resto = carroNumero % pessoaNumero;
+		
+		JOptionPane.showMessageDialog(null, "Divisão para pessoas deu: " + divisao + " carros e sobrou " + resto + " carro(s)");
 	}
 
 }
--- primeiroProgramaJava/src/module-info.java
+++ primeiroProgramaJava/src/module-info.java
@@ -6,4 +6,5 @@
  *
  */
 module primeiroProgramaJava {
+	requires java.desktop;
 }
